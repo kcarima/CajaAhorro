48299c33d8336ec748a3468cbf3005c609ad39dd
Miguel Romero <mjromero978@gmail.com>
2024-11-13 17:12:53 -0400

    inicio conceptos y tipo conceptos

--- database/migrations/2024_11_12_231048_create_archivo_sinu_detalle.php
+++ database/migrations/2024_11_12_231048_create_archivo_sinu_detalle.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('archivo_sinu_detalle', function (Blueprint $table) {
             $table->id();
             $table->foreignId(column: 'archivo_sinu_table_id')->references(column: 'id')->on(table: 'sca.archivo_sinu_table')->onUpdate(action: 'cascade')->onDelete(action: 'restrict');
+            $table->foreignId('socio_id')->references('id')->on('sca.socios')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('concepto_id')->default(0);
+            $table->decimal(column: 'monto', total: 16, places: 2)->default(value: 0.00);
             $table->timestamps();
         });
     }
