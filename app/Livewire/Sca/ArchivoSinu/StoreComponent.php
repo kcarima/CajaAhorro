919cef993dc86348b26113d1cbd0b5320aabb31a
Miguel Romero <mjromero978@gmail.com>
2024-11-19 05:57:06 -0400

    carga de detalle del archivo

--- app/Livewire/Sca/ArchivoSinu/StoreComponent.php
+++ app/Livewire/Sca/ArchivoSinu/StoreComponent.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Livewire\Sca\ArchivoSinu;
+use App\Models\SCA\ArchivoSinu;
+use App\Models\SCA\Socio;
+use App\Models\SCA\conceptos;
 
 use Livewire\Component;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
 
 class StoreComponent extends Component
 {
+    use WithFileUploads;
     public $isOpen      = 0;
 
     public $archivo_id   = 0;
     public $fecha       = '';
     public $descripcion = '';
+    public $file;
 
 
     public function render()
@@ -19,10 +29,59 @@ class StoreComponent extends Component
     }
 
     public function openModal(){
+        if ( $this->archivo_id == 0 ){
+            $this->fecha = Carbon::now()->format('Y-m-d');
+        }
         $this->isOpen=true;
     }
 
     public function closeModal(){
         $this->isOpen=false;
+    }
+
+    public function store(Request $request){
+        $this->validate([
+            'fecha' => 'required|date',
+            'descripcion' => ['required'],
+            'file' => 'required|max:2048',
+            //'file' => 'required|mimes:txt|max:2048',
+        ], [
+            'fecha.required' => 'Debe indicar Fecha.',
+            'descripcion.required' => 'Debe indicar Descripción.',
+            'file.required' => 'Debe Cargar un archivo .txt',
+            //'file.mimes' => 'El archivo debe ser un .txt',
+            //'file.max' => 'El tamaño máximo del Archivo debe ser de 2MB.',
+        ]);
+
+        session()->flash('message',  'Subiendo Archivo...');
+        
+        $query = ArchivoSinu::create([
+            'fecha' => $this->fecha,
+            'descripcion' => $this->descripcion,
+            'status' => 0
+        ]);
+        $this->file->storeAs('txt', $query->id.'.txt');
+        session()->flash('message',  'Cargando Archivo...');
+
+        $contents = Storage::disk('txt')->get($query->id.'.txt');
+        $lineas = explode("\n",trim($contents));
+        $contador_nosocios = 0;
+        $contador_noconcep = 0;
+        foreach( $lineas as $linea ){
+            $detalle_linea = explode(";",$linea);
+            // Buscar el socio por su ficha
+            $socio = Socio::where('ficha', $detalle_linea[1])->first();
+            // Verificar si se encontró al socio
+            if (!$socio) {                
+                $contador_nosocios++;
+            }
+        
+            // Verificar si se encontró el concepto
+            $concepto = conceptos::where('codigo', $detalle_linea[4])->first();
+            if (!$concepto) {
+                $contador_noconcep++;
+            }
+        }
+        dd("Cant NoSocios: $contador_nosocios, Cant. NoConceptos:  $contador_noconcep.");
     }
 }
--- resources/views/livewire/sca/archivo-sinu/store-component.blade.php
+++ resources/views/livewire/sca/archivo-sinu/store-component.blade.php
@@ -1,4 +1,7 @@
 <div>
+    @php
+        use Carbon\Carbon;
+    @endphp
     <div class="w-full flex justify-end">
         <button wire:click="openModal" class="flex gap-2 rounded-md bg-white shadow-md p-2 items-center mb-4 hover:shadow-lg hover:cursor-pointer" style="font-size: 18 !important;">
             <b class="text-blue-700">+</b><span class="text-gray-700">Agregar</span>
@@ -18,20 +21,30 @@
                                 <div class="grid 2xl:grid-cols-5 md:grid-cols-2 sm:grid-cols-2 grid-cols-2 items-center gap-2 mx-2 mt-2" >
                                     <div>
                                         <x-label for="fecha" value="Fecha:" />
-                                        <x-input class="w-full" type="text" wire:model.live="fecha" required id="fecha" name="fecha"/>
-                                        @error('codigo')
-                                            <span class="text-red-500">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div>
-                                        <x-label for="descripcion" value="Descripcion:" />
-                                        <x-input class="w-full" type="text" wire:model.live="descripcion" required id="descripcion" name="descripcion"/>
-                                        @error('descripcion')
+                                        <x-input type="date"  pattern="\d{2}-\d{2}-\d{4}" placeholder="DD-MM-AAAA" wire:model.live="fecha" style="font-size: 12 !important;" value="" required id="fecha" name="fecha"/>
+                                        @error('fecha')
                                             <span class="text-red-500">{{ $message }}</span>
                                         @enderror
                                     </div>                                    
                                 </div>
-
+                                <div class="grid 2xl:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 items-center gap-2 mx-2 mt-2" >
+                                    <div>
+                                        <x-label for="descripcion" value="Descripcion:" />
+                                        <x-input class="w-full" type="text" wire:model.live="descripcion" required id="descripcion" name="descripcion" style="width: 100% !important;"/>
+                                        @error('descripcion')
+                                            <span class="text-red-500">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="grid 2xl:grid-cols-5 md:grid-cols-2 sm:grid-cols-2 grid-cols-2 items-center gap-2 mx-2 mt-2" >
+                                    <input type="file" wire:model="file" value="{{ __('Seleccionar Archivo') }}"/>
+                                    @error('file') 
+                                        <span class="error">{{ $message }}</span> 
+                                    @enderror
+                                </div>
+                                @if (session('message'))
+                                    <h1 class="center" style="text-color:red !important;"> {{session('message')}}</h1>                                    
+                                @endif
                                 <br>
                                 <hr class="my-4  items-center " style="border: 1px solid #ccc;">                                
                                 <div class="flex justify-around items-center flex-col-reverse lg:flex-row mb-4 gap-2 mt-4">
